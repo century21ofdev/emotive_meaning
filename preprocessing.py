62924550b997134e2c81e2fd596a42ec2cbb25ab
Recep <recep4sirin@gmail.com>
2021-05-08 19:57:08 +0300

    Add pre-processing for nlp

---
+++ preprocessing.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from utils import remove_punctuation, raw_data_empty_info, contractions, emoticons
+
+tokenizer = RegexpTokenizer(r'\w+')
+lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
+
+df = pd.read_csv('E-Commerce_Reviews.csv')
+raw_data_analysis = raw_data_empty_info(df)
+
+# load emoticons
+emoticons = emoticons()
+# load contractions
+contractions = contractions()
+
+if raw_data_analysis.review:
+    # Removing empty reviews row if there are
+    df.dropna(subset=['Review'], inplace=True)
+
+df['Review'] = df['Review'].apply(lambda x: " ".join([emoticons[i] if i in emoticons else i for i in x.split()]))
+df['Review'] = df['Review'].apply(lambda x: " ".join([contractions[i] if i in contractions else i for i in x.split()]))
+df['Review'] = df['Review'].apply(lambda x: remove_punctuation(x))
+df['Review'] = df['Review'].apply(lambda x: tokenizer.tokenize(x.lower()))
+df['Review'] = df['Review'].apply(lambda x: [w for w in x if not w in set(stopwords.words('english'))])
+df['Review'] = df['Review'].apply(lambda x: [lemmatizer.lemmatize(i) for i in x])
+# df['Review'] = df['Review'].apply(lambda x: " ".join([stemmer.stem(i) for i in x]))
