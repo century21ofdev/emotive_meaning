d7ce8aa1cef185638e2e325daf44d691117c59d9
Recep <recep4sirin@gmail.com>
2021-05-08 23:36:06 +0300

    Added word cloud and Applied refactoring

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,6 +1,17 @@
 import numpy as np
 from nltk.probability import FreqDist
-from collections import namedtuple
+from collections import namedtuple, Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+def _reviews(df):
+    words = list()
+
+    for i in df["Review"]:
+        for j in i:
+            words.append(j)
+    return words
 
 
 def bow(df):
@@ -15,15 +26,26 @@ def bow(df):
 
 def frequency_distribution(df, len_fd: int = 10):
     """frequency distribution"""
-    fd = namedtuple("fd", ["most_common", "chart", "all"])
-    words = list()
 
-    for i in df["Review"]:
-        for j in i:
-            words.append(j)
+    fd = namedtuple("fd", ["most_common", "chart", "all"])
+    words = _reviews(df)
 
     f_dist = FreqDist(words)
     fd.all = f_dist
     fd.most_common = f_dist.most_common(len_fd)
     fd.chart = f_dist.plot(10)
     return fd
+
+
+def word_cloud(df):
+    """words & tags cloud from most common frequencies"""
+
+    words = _reviews(df)
+    word_could_dict = Counter(words)
+
+    wc = WordCloud().generate_from_frequencies(word_could_dict)
+
+    plt.figure(figsize=(12, 12))
+    plt.imshow(wc)
+    plt.axis("off")
+    plt.show()
