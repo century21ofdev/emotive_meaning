9a34ab6782bc7d98121872835d7ba4d775f615b7
Recep <recep4sirin@gmail.com>
2021-05-09 16:20:16 +0300

    optimized pre-processing's steps

--- preprocessing.py
+++ preprocessing.py
@@ -20,11 +20,32 @@ contractions = contractions()
 if raw_data_analysis.review:
     # Removing empty reviews row if there are
     df.dropna(subset=['Review'], inplace=True)
+    df['Title'].fillna(value="No Title", inplace=True)
 
 df['Review'] = df['Review'].apply(lambda x: " ".join([emoticons[i] if i in emoticons else i for i in x.split()]))
 df['Review'] = df['Review'].apply(lambda x: " ".join([contractions[i] if i in contractions else i for i in x.split()]))
 df['Review'] = df['Review'].apply(lambda x: remove_punctuation(x))
-df['Review'] = df['Review'].apply(lambda x: tokenizer.tokenize(x.lower()))
+# df['Review'] = df['Review'].apply(lambda x: tokenizer.tokenize(x.lower()))
 df['Review'] = df['Review'].apply(lambda x: [w for w in x if not w in set(stopwords.words('english'))])
-df['Review'] = df['Review'].apply(lambda x: [lemmatizer.lemmatize(i) for i in x])
+# df['Review'] = df['Review'].apply(lambda x: [lemmatizer.lemmatize(i) for i in x])
 # df['Review'] = df['Review'].apply(lambda x: " ".join([stemmer.stem(i) for i in x]))
+
+
+def pipeline_1():
+    """Tokenized & Lemmatized"""
+    df['Review'] = df['Review'].apply(lambda x: tokenizer.tokenize(x.lower()))
+    df['Review'] = df['Review'].apply(lambda x: [lemmatizer.lemmatize(i) for i in x])
+    return df
+
+
+def pipeline_2():
+    """Lemmatized"""
+    df['Review'] = df['Review'].apply(lambda x: [lemmatizer.lemmatize(i) for i in x])
+    return df
+
+
+def pipeline_3():
+    """Tokenized & Stemmed"""
+    df['Review'] = df['Review'].apply(lambda x: tokenizer.tokenize(x.lower()))
+    df['Review'] = df['Review'].apply(lambda x: " ".join([stemmer.stem(i) for i in x]))
+    return df
